eecf08f9ba9aa4babfedcbdfef8ce92d548ebe0e
Gary Kane <gakane@bu.edu>
2021-02-28 11:57:51 -0500

    fix bug in here on command line

--- R/here.R
+++ R/here.R
@@ -9,6 +9,8 @@ here <- function() {
     dirname(rstudioapi::getActiveDocumentContext()$path)
   } else {
     f_ind = which(grepl("file", args))
-    file.path(getwd(), dirname(gsub("--file=", "", args[f_ind])))
+    fp = file.path(getwd(), dirname(gsub("--file=", "", args[f_ind])))
+    if (substring(fp, nchar(fp)) == ".") fp = substring(fp, 1, nchar(fp)-2)
+    fp
   }
 }
